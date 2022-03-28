395fe313b4424e2450901ee279837440e9da7c23
Griffin Alcorn <griffin.alcorn@gmail.com>
2022-03-28 13:29:42 -0400

    started methods for validating commands. json file will hold urls to responses.

--- bot.js
+++ bot.js
@@ -1,44 +1,85 @@
 const { Client, Intents, ChannelManager } = require('discord.js');
+const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
+//Bot and client data setup
+const BOT_COMMANDS = JSON.parse(fs.readFileSync('commands.json'));
+const BOT_PREFIX = '!wojak';
+const VALID_LIST = `Valid commands are 'wojak', 'soy', 'doomer', 'gigachad', 'chad', 'virgin'`;
 const BOT_INTENTS = new Intents([
   Intents.FLAGS.GUILDS,
   Intents.FLAGS.GUILD_MEMBERS,
   Intents.FLAGS.GUILD_MESSAGES,
   Intents.FLAGS.GUILD_WEBHOOKS
 ]);
-const client = new Client({ intents: BOT_INTENTS });
-//const channelMgr = new ChannelManager();
 
+//Client setup and login
+const client = new Client({ intents: BOT_INTENTS });
+client.login(process.env.TOKEN);
+
+//Confirm login on console
 client.on('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}`);
-  // const channel = await (client.channels.cache.get(process.env.GUILD_ID));
-  // //console.log(channel)
-  // //await channel.send('content');  
-  // const user = await client.users.fetch('id');
-  // user.send('content');
-  //console.log(client.channels.cache)
+  console.log(`Logged in as ${client.user.tag} at ${Date()}`);
 });
 
+//Respond to slash command
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
+  if (!interaction.isCommand()) 
+    return;
   if (interaction.commandName === 'wojak') {
     await interaction.reply('yo!');
-    interaction.followUp('asdf')
-    await interaction.user.send('hello')
   } else {
-    await interaction.reply(`Sorry, didn't understand that`);
-  }
-  
-
+    return;
+  }  
 });
 
+//Event listener for messages sent within server
 client.on('messageCreate', async (msg) => {
-  await msg.reply({ content: 'content', allowedMentions: { repliedUser: false }})
-  //msg.channel.send('response');
-  console.log('msg received');
+  if (!isBotMessage(msg)) {
+    console.log('human msg received');
+    await validateMessage(msg)
+      .then((data) => {
+        console.log(`data: ${data}`)
+        console.log('valid command received')    
+        msg.reply({ content: 'valid command received'});
+      })
+      .catch((e) => {
+        console.log(`invalid command received: ${e.message}`);
+      });
+  }
 });
 
-client.login(process.env.TOKEN);
+const isBotMessage = (msg) => {
+  //Returns false when message is sent by human
+  if (msg.webhookId || msg.author.bot) 
+    return true;
+  else
+    return false;
+}
+
+const validateMessage = async (msg) => {
+  //Returns true when command is correctly formatted
+  if (msg.content.startsWith(BOT_PREFIX)) {
+    //Command initiator is correct; let's check what type of wojak to send
+    const options = msg.content.split(' ')
+
+    if (options.length > 2) {
+      throw new Error('Too many options selected')
+    } else if ( options[1] === 'help'){
+      return VALID_LIST;
+    } else {
+      return validateCommand(options[1].toLowerCase());
+    }
+    
+  } else {
+    throw new Error('Message did not start with !wojak');
+  }
+}
+
+const validateCommand = async (command) => {
+  if(BOT_COMMANDS[command]) {
+    return command;
+  } else
+    throw new Error(`Not found on the command list. \n${VALID_LIST}`)
+}
