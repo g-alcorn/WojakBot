90875c488a95a6419a390602fb49dfc2a871a49e
Griffin Alcorn <griffin.alcorn@gmail.com>
2022-05-09 14:31:19 -0400

    cleanup console messages, add new commands

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
-//Port Management
-const port = process.env.PORT || 3333;
-console.log(port);
-
 //Bot and client data setup
 const BOT_COMMANDS = JSON.parse(fs.readFileSync('commands.json'));
 const BOT_HELP = fs.readFileSync('help.txt').toString();
@@ -25,9 +21,6 @@ client.login(process.env.TOKEN);
 //Confirm login on console
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag} at ${Date()}`);
-  if(BOT_HELP){
-    console.log('help loaded')
-  }
 });
 
 //Respond to slash command
@@ -35,25 +28,25 @@ client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) 
     return;
   if (interaction.commandName === 'wojak') {
-
     await interaction.reply(BOT_COMMANDS.wojak.default);
   } else {
     return;
-  }  
+  }
 });
 
-//Event listener for messages sent within server
+// Event listener for messages sent within server
 client.on('messageCreate', async (msg) => {
   if (!isBotMessage(msg)) {
     await validateMessage(msg)
       .then((data) => {
-        console.log(`'${data}' is a valid command, sending data`);
+        // Executed when valid command has been sent
+        // data is the image URL
 
-        //Manage help requests
+        // Manage help requests
         if(data === 'help') {
           msg.author.send(BOT_HELP);
-        } else {
-          //Serve the meme
+        } else if (data) {
+          // Serve the meme
           msg.reply({ content: data});
         }
       })
@@ -64,7 +57,8 @@ client.on('messageCreate', async (msg) => {
 });
 
 const isBotMessage = (msg) => {
-  //Returns false when message is sent by human
+  // Returns true when message is sent by a bot
+  // Returns false when message is sent by a human
   if (msg.webhookId || msg.author.bot) 
     return true;
   else
@@ -75,41 +69,42 @@ const validateMessage = async (msg) => {
   //Returns true when command is correctly formatted
   if (msg.content.startsWith(BOT_PREFIX)) {
     //Command initiator is correct; let's check what type of wojak to send
-    const options = msg.content.split(' ')
+    const options = msg.content.split(' ');
     
     if (options.length > 3) {
       throw new Error('Too many options selected')
     } else if (options.length === 1) {
-      console.log('default option')
+      // Default selected
       return validateCommand('default');
-    } else if(options.length === 2 && options[1] !== 'help') {
-      console.log('1 option')
+    } else if (options.length === 2 && options[1] !== 'help') {
+      // Category chosen but not subcategory
       return validateCommand(options[1].toLowerCase());
     } else if (options.length === 2 && options[1] === 'help') {
+      // Help message selected
       return options[1];
     } else if (options.length === 3) {
+      // Category and subcategory chosen
       return validateCommand([options[1].toLowerCase(), options[2].toLowerCase()]);
-    } else if (options.length > 3) {
-      throw new Error('Too many options!');
     }
   } else {
-    throw new Error('Message did not start with !wojak');
+    // Message was not a command
+    return false;
   }
 }
 
 const validateCommand = async (command) => {
   if(Array.isArray(command) && command.length === 2 && BOT_COMMANDS[command[0]]) {
-    console.log('response category and subcategory chosen')
-    if(BOT_COMMANDS[command[0]][command[1]]) {
+    // Category and subcategory are chosen
+    if (BOT_COMMANDS[command[0]][command[1]]) {
       return BOT_COMMANDS[command[0]][command[1]];
     } else {
       throw new Error(`Not found on the command list.`)
     }
-  } else if(BOT_COMMANDS[command]) {
-    console.log('response category chosen')
+  } else if (BOT_COMMANDS[command]) {
+    // Category is chosen but not subcategory - send default for category
     return BOT_COMMANDS[command].default;
-  } else if(command === 'default') {
-    console.log('no category chosen, sending default')
+  } else if (command === 'default') {
+    // No category is chosen - send global default
     return BOT_COMMANDS.wojak.default;
   } else
     throw new Error(`Not found on the command list.`)
